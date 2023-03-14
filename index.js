49725658f8b576cff30e66504463354fc2d78c1c
Tyiss Goodman <sticktyiss@gmail.com>
2023-03-14 09:29:56 -0700

    Changed function and if/else to turnary

--- index.js
+++ index.js
@@ -2,21 +2,9 @@ function containsVowel(str){
   let vowel = 0
   let con = 0  
   for(let i=0; i<str.length; i++ ){
-    if("aeiou".indexOf(str[i].toLowerCase()) != -1) {
-      vowel++
-    } else {
-      con++
-    }
+    "aeiou".includes(str[i].toLowerCase()) ? vowel++ : con++
   }
-  if(str==='') {
-    console.log(`Please enter a word to be evaluated`)
-  } else if(vowel===con) {
-    console.log(`${str} has an equal number of vowels and consonants`)
-  } else if(vowel>con) {
-    console.log(`There are more vowels in ${str} than consonants`)
-  } else {
-    console.log(`There are more consonants in ${str} than vowels`)
-  }
+  str==='' ? console.log(`Please enter a word to be evaluated`) : vowel===con ? console.log(`${str} has an equal number of vowels and consonants`) : vowel>con ? console.log(`There are more vowels in ${str} than consonants`) : console.log(`There are more consonants in ${str} than vowels`)
 };
 
-containsVowel('beAnoUt')
+containsVowel('')
